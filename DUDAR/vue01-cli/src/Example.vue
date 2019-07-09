93339a3bcadfc6fcdba4762b49a41d38796b5577
n-drew <n-drew@list.ru>
2019-07-09 13:17:36 +0300

    dudar 8 lesson complete

---
+++ DUDAR/vue01-cli/src/Example.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Пробный текст</h1>
+        <app-input></app-input>
+    </div>
+</template>
+
+<script>
+    import Input from './Field.vue';
+
+    export default {
+        components: {
+            'app-input': Input
+        }
+    }
+</script>
--- DUDAR/vue01-cli/src/main.js
+++ DUDAR/vue01-cli/src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Example from './Example.vue'
+
 
 Vue.config.productionTip = false
+
+Vue.component ('app-example', Example)
 
 new Vue({
   render: h => h(App),
