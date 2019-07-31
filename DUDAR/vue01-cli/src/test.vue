3b5eb0e5f1187805da39cd39aaef02045d134b56
n-drew <n-drew@list.ru>
2019-07-31 16:54:22 +0300

    Update test.vue
    убрал проверки

--- DUDAR/vue01-cli/src/test.vue
+++ DUDAR/vue01-cli/src/test.vue
@@ -3,8 +3,8 @@
         <h1>Введите имя фильма</h1>
         <input type="text" v-model='userInput'>
         <button @click="show = !show">{{buttonText}}</button>
-        <p v-if="show"> {{ readyString }} </p>
-        <!-- <button @click="doString(addStringEng)" v-if="caption='addStringEng'">На английском</button> -->
+        <p v-if="show"> {{ userInput + addStringEng }} </p>
+        <p v-else> {{ userInput + addStringRus }} </p>
     </div>
 </template>
 
@@ -13,26 +13,25 @@
 export default {
     data () {
         return {
-            show: true,
+            show: false,
             userInput: '',
-            readyString: '',
             addStringRus: ' в твоей заднице.',
             addStringEng: ' in your ass.'
         }
     },
-    methods: {
-        doString(addString) {
-            if (this.userInput) {
-                this.readyString = this.userInput + addString
-            }
-            else {
-                alert("Введите название фильма!")
-                }
-
-            //console.log(this.userInput);
-            //console.log(readyStr);
-        }
-    },
+   // methods: {
+   //     doString(addString) {
+   //        if (this.userInput) {
+   //             this.readyString = this.userInput + addString
+   //         }
+   //         else {
+   //             alert("Введите название фильма!")
+   //             }
+//
+  //          //console.log(this.userInput);
+  //          //console.log(readyStr);
+  //      }
+  //  },
     computed: {
         buttonText: function() {
             if (this.show) {
